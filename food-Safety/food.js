b53d25d7c44c32843281160ebc9588df39a737c0
Lester-D <neoletodeswati@gmail.com>
2022-11-20 19:01:11 +0800

    Pushing all This is not finish :D

---
+++ food-Safety/food.js
@@ -0,0 +1,24 @@
+const buzzer = document.querySelectorAll("#buzz")
+const bells = document.querySelectorAll("#bell")
+const openWindows= document.querySelectorAll("#openWindow")
+const closeWindows= document.querySelectorAll("#CloseWindow")
+
+buzzer.forEach(button => button.addEventListener 
+  ("click", () => {
+
+    let audio = new Audio("/pictures/buzz.wav");
+    audio.play()
+
+} ));
+
+bells.forEach(button => button.addEventListener 
+  ("click", () => {
+
+    let audio = new Audio("/pictures/bell.wav");
+    audio.play()
+
+} ));
+
+
+
+
