d6f3d20737ac76d8cbac03e7004bd179aa1513b6
Lester-D <neoletodeswati@gmail.com>
2022-11-20 23:04:45 +0800

    working na :D design nalang :D

--- food-Safety/food.js
+++ food-Safety/food.js
@@ -17,8 +17,65 @@ bells.forEach(button => button.addEventListener
     let audio = new Audio("pictures/bell.wav");
     audio.play()
 
+    
+
 } ));
 
+function toggle(){
+var x = document.getElementById ("pics3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+  else {
+    x.style.display = "none"
+  }
+}
+
+function toggle1(){
+var x1 = document.getElementById ("pics4");
+  if (x1.style.display === "none") {
+    x1.style.display = "block";
+  }
+  else {
+    x1.style.display = "none"
+  }
+}
+function toggle2(){
+var x1 = document.getElementById ("pics5");
+  if (x1.style.display === "none") {
+    x1.style.display = "block";
+  }
+  else {
+    x1.style.display = "none"
+  }
+}
+function toggle3(){
+var x1 = document.getElementById ("pics6");
+  if (x1.style.display === "none") {
+    x1.style.display = "block";
+  }
+  else {
+    x1.style.display = "none"
+  }
+}
+function toggle4(){
+var x1 = document.getElementById ("pics7");
+  if (x1.style.display === "none") {
+    x1.style.display = "block";
+  }
+  else {
+    x1.style.display = "none"
+  }
+}
+function toggle5(){
+var x1 = document.getElementById ("pics8");
+  if (x1.style.display === "none") {
+    x1.style.display = "block";
+  }
+  else {
+    x1.style.display = "none"
+  }
+}
 
 
 
