6f22f6a4f066f39b9b357eeda1690b39d2d22764
Lester-D <neoletodeswati@gmail.com>
2022-11-21 13:35:24 +0800

    editing the padding and spacing

--- food-Safety/food.js
+++ food-Safety/food.js
@@ -1,8 +1,7 @@
 const buzzer = document.querySelectorAll("#buzz")
 const bells = document.querySelectorAll("#bell")
-const openWindows= document.querySelectorAll("#openWindow")
-const closeWindows= document.querySelectorAll("#CloseWindow")
 
+//Audio
 buzzer.forEach(button => button.addEventListener 
   ("click", () => {
 
@@ -16,66 +15,75 @@ bells.forEach(button => button.addEventListener
 
     let audio = new Audio("pictures/bell.wav");
     audio.play()
-
-    
-
+      
 } ));
 
-function toggle(){
-var x = document.getElementById ("pics3");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  }
-  else {
-    x.style.display = "none"
-  }
-}
+//End Audio
 
-function toggle1(){
-var x1 = document.getElementById ("pics4");
-  if (x1.style.display === "none") {
-    x1.style.display = "block";
-  }
-  else {
-    x1.style.display = "none"
-  }
-}
-function toggle2(){
-var x1 = document.getElementById ("pics5");
-  if (x1.style.display === "none") {
-    x1.style.display = "block";
-  }
-  else {
-    x1.style.display = "none"
-  }
-}
-function toggle3(){
-var x1 = document.getElementById ("pics6");
-  if (x1.style.display === "none") {
-    x1.style.display = "block";
-  }
-  else {
-    x1.style.display = "none"
-  }
-}
-function toggle4(){
-var x1 = document.getElementById ("pics7");
-  if (x1.style.display === "none") {
-    x1.style.display = "block";
-  }
-  else {
-    x1.style.display = "none"
-  }
-}
-function toggle5(){
-var x1 = document.getElementById ("pics8");
-  if (x1.style.display === "none") {
-    x1.style.display = "block";
-  }
-  else {
-    x1.style.display = "none"
-  }
-}
+bells.forEach(button => button.addEventListener 
+  ("click", () => {
+
+    {
+    var x = document.getElementById ("pics3");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      }
+      else {
+        x.style.display = "none"
+      }
+    }
+      
+{
+
+      var x1 = document.getElementById ("pics4");
+        if (x1.style.display === "none") {
+          x1.style.display = "block";
+        }
+        else {
+          x1.style.display = "none"
+        }
+      }
+
+        {
+        var x1 = document.getElementById ("pics5");
+          if (x1.style.display === "none") {
+            x1.style.display = "block";
+          }
+          else {
+            x1.style.display = "none"
+          }
+        }
+
+        {
+        var x1 = document.getElementById ("pics6");
+          if (x1.style.display === "none") {
+            x1.style.display = "block";
+          }
+          else {
+            x1.style.display = "none"
+          }
+        }
+
+        {
+        var x1 = document.getElementById ("pics7");
+          if (x1.style.display === "none") {
+            x1.style.display = "block";
+          }
+          else {
+            x1.style.display = "none"
+          }
+        }
+        
+        {
+        var x1 = document.getElementById ("pics8");
+          if (x1.style.display === "none") {
+            x1.style.display = "block";
+          }
+          else {
+            x1.style.display = "none"
+          }
+        }
+} ));
 
 
 
