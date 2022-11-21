0e3234a5692bde888cc1111edcf31041ac4742ee
Lester-D <neoletodeswati@gmail.com>
2022-11-21 17:52:27 +0800

    Working Hidden

--- food-Safety/food.js
+++ food-Safety/food.js
@@ -20,70 +20,59 @@ bells.forEach(button => button.addEventListener
 
 //End Audio
 
-bells.forEach(button => button.addEventListener 
-  ("click", () => {
-
-    {
-    var x = document.getElementById ("pics3");
-      if (x.style.display === "none") {
-        x.style.display = "block";
-      }
-      else {
-        x.style.display = "none"
-      }
+//HIDE FUNCTION
+function hide1 () {
+  var x = document.getElementById ("pics3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
     }
-      
-{
-
-      var x1 = document.getElementById ("pics4");
-        if (x1.style.display === "none") {
-          x1.style.display = "block";
-        }
-        else {
-          x1.style.display = "none"
-        }
-      }
-
-        {
-        var x1 = document.getElementById ("pics5");
-          if (x1.style.display === "none") {
-            x1.style.display = "block";
-          }
-          else {
-            x1.style.display = "none"
-          }
-        }
-
-        {
-        var x1 = document.getElementById ("pics6");
-          if (x1.style.display === "none") {
-            x1.style.display = "block";
-          }
-          else {
-            x1.style.display = "none"
-          }
-        }
-
-        {
-        var x1 = document.getElementById ("pics7");
-          if (x1.style.display === "none") {
-            x1.style.display = "block";
-          }
-          else {
-            x1.style.display = "none"
-          }
-        }
-        
-        {
-        var x1 = document.getElementById ("pics8");
-          if (x1.style.display === "none") {
-            x1.style.display = "block";
-          }
-          else {
-            x1.style.display = "none"
-          }
-        }
-} ));
-
-
-
+    else {
+      x.style.display = "none"
+    }
+  }
+function hide2 () {
+  var x = document.getElementById ("pics4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else {
+      x.style.display = "none"
+    }
+  }
+function hide3 () {
+  var x = document.getElementById ("pics5");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else {
+      x.style.display = "none"
+    }
+  }
+function hide4 () {
+  var x = document.getElementById ("pics6");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else {
+      x.style.display = "none"
+    }
+  }
+function hide5 () {
+  var x = document.getElementById ("pics7");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else {
+      x.style.display = "none"
+    }
+  }
+function hide6 () {
+  var x = document.getElementById ("pics8");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else {
+      x.style.display = "none"
+    }
+  }
+// END of HIDE FUNCTION
